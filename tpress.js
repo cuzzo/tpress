47265500bdd3b7dbf7e16acb9fabf0d781e0ddb5
Cuzzo <yahn007@outlook.com>
2013-09-06 23:17:53 -0400

    Removed destructive logs.

--- tpress.js
+++ tpress.js
@@ -17,16 +17,13 @@ function read_files(files) {
   var file_data = {};
   _.each(files, function(file) {
     var data = fs.readFileSync(file, "utf8");
-    console.log(data);
     file_data[file] = data;
   });
   return file_data;
 }
 
 function main(argc, argv) {
-  console.log(argv[2]);
   get_files(argv[2]).done(function(files) {
-    console.log('files', files);
     file_data = read_files(files);
     console.log(JSON.stringify(file_data));
   });
