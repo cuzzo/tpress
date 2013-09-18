d78bc1abe9e78a93daf25f0445c0b5c2084c0a74
Cuzzo <yahn007@outlook.com>
2013-09-18 10:14:28 -0700

    Create path to options.output if it does not exist.

--- tasks/tpress.js
+++ tasks/tpress.js
@@ -2,6 +2,8 @@
 
 module.exports = function(grunt) {
   var fs = require("fs");
+  var path = require("path");
+  var mkdirp = require("mkdirp");
   var tpress = require("../tpress");
 
   grunt.registerTask("tpress", "Compress your files", function() {
@@ -13,8 +15,16 @@ module.exports = function(grunt) {
 
     glob_promise.done(function(glob_results) {
       var json = tpress.press_glob_results(glob_results, min_options);
-      fs.writeFile(options.output, json, function() {
-        done();
+      mkdirp(path.dirname(options.output), function(err) {
+        if (err) {
+          grunt.log.error("Could not create path to " + options.output);
+        }
+        fs.writeFile(options.output, json, function(err) {
+          if (err) {
+            grunt.log.error("Could not write to file " + options.output);
+          }
+          done();
+        });
       });
     });
   });
